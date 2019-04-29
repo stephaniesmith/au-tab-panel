38604cf95f56da3cbd1e38381e2597c54579e2b2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-04-29 08:43:40 -0700

    working on tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,57 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { AuTabPanelComponent } from './au-tab-panel/au-tab-panel.component';
+import { AuTabComponent } from './au-tab/au-tab.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let el: DebugElement;
+  let tabPanel: DebugElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AuTabPanelComponent,
+        AuTabComponent
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+    tabPanel = el.query(By.css('#tab-panel'));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  })
+
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
   }));
+
+  it('should only find one tab', () => {
+    const tabs = tabPanel.queryAll(By.css('.tab'));
+
+    expect(tabs).toBeTruthy();
+    expect(tabs.length).toBe(1);
+  })
+
+  it('should only find one tab', () => {
+    const selectedButton = tabPanel.query(By.css('.tab-panel-buttons li.selected')).nativeElement;
+
+    expect(selectedButton).toBeTruthy();
+    expect(selectedButton.textContent).toContain('Contact');
+  })
+
+  it('should only find one tab', () => {
+    const contactEmail = tabPanel.query(By.css('.contact-email'));
+
+    expect(contactEmail).toBeTruthy();
+  })
 });
